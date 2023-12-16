3d63deda7bf0652521c6dc8c6fcc76a799496a7f
maroon <marooncoucal@gmail.com>
2023-12-16 14:12:14 +0300

    page Nav 16 12 23

--- App.js
+++ App.js
@@ -16,8 +16,8 @@ import Card from './components/card';
 import Search from './components/search';
 import Tag from './components/tag';
 import CardButton from './components/cardAction';
-import ShortCardList from './components/shortCard';
-import { Heading1, TagText, TextLink } from './components/text';
+import ShortCardList, { ShortCard1, ShortCard2, ShortCard3, ShortCard4 } from './components/shortCard';
+import { Heading1, Heading2, MainText, TagText, TextLink } from './components/text';
 
 const Stack = createNativeStackNavigator()
 const BottomTab = createBottomTabNavigator()
@@ -25,46 +25,52 @@ const Drawer = createDrawerNavigator()
 const MaterialBottomTabs = createMaterialBottomTabNavigator()
 const MaterialTopTabs = createMaterialTopTabNavigator()
 
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator style={{paddingTop:40}}>
-        <Drawer.Screen name="home" component={Home} options={{headerShown:true}} />
-        {/* <Drawer.Screen name="search result" component={Screen1} /> */}
-        <Drawer.Screen name="search" component={Screen2} options={{headerShown:false}}/>
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator style={{paddingTop:0}}>
+          <Stack.Screen name="home" component={Home} options={{headerShown:false}} />
+
+          {/* <Stack.Screen name="search result" component={Screen1} /> */}
+          {/* <Stack.Screen name="search" component={SearchScreen} options={{headerShown:false}}/> */}
+
+          <Stack.Screen name='cardList' component={CardListScreen}></Stack.Screen>
+          <Stack.Screen name='card' component={CardScreen}></Stack.Screen>
+
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
 
 function Home({navigation}){
   return (
     <ScrollView style={{width:'100%'}}>
-      <View style={styles.container}>
-          
-
-            {/* <Button title='Экран 1' onPress={() => navigation.navigate('screen1')} /> */}
+      <View style={{...styles.container, paddingTop:40}}>
 
             <View style={{ width:"auto", gap:8, paddingVertical:10,
             flexDirection:"row", alignItems:'center', justifyContent:'center'}}>
-                <Pressable onPress={() => navigation.navigate('search')}>
+                <Pressable onPress={() => navigation.navigate('cardList')}>
                   <Search></Search>
                 </Pressable>
-                <Pressable onPress={() => navigation.navigate('search')}>
+                <Pressable onPress={() => navigation.navigate('cardList')}>
                     <Image source={require('./assets/random.png')} style={{width:36, height:36}}/>
                 </Pressable>
             </View>
             
-
             <Heading1>Для Вас</Heading1>
-            <Card 
-              pic={require('./assets/aboutface.png')}
-              title={"Об интерфейсах: основы интерактивного дизайна"}
-              text={"«Об интерфейсах» — это книга, которая вывела интерактивный дизайн из исследовательских лабораторий в повседневный лексикон, а обновленное четвертое издание продолжает лидировать в идеях и методах, актуальных для сегодняшних практиков дизайна и разработчиков"}
-              tag1={"веб-дизайн"}
-              tag2={"книга"}
-            ></Card>
-            
+            <Pressable onPress={() => {navigation.navigate('card', {id:4})}}>
+              <Card 
+                pic={require('./assets/aboutface.png')}
+                title={"Об интерфейсах: основы интерактивного дизайна"}
+                text={"«Об интерфейсах» — это книга, которая вывела интерактивный дизайн из исследовательских лабораторий в повседневный лексикон, а обновленное четвертое издание продолжает лидировать в идеях и методах, актуальных для сегодняшних практиков дизайна и разработчиков"}
+                tag1={"веб-дизайн"}
+                tag2={"книга"}
+              ></Card>
+            </Pressable>
             
             <Heading1>Выбор редакции</Heading1>
             <Card
@@ -76,7 +82,18 @@ function Home({navigation}){
             ></Card>
 
             <Heading1>Топ Запросы</Heading1>
-            <ShortCardList></ShortCardList>
+            <Pressable onPress={() => {navigation.navigate('card', {id:1})}}>
+              <ShortCard1></ShortCard1>
+            </Pressable>
+            <Pressable onPress={() => {navigation.navigate('card', {id:2})}}>
+              <ShortCard2></ShortCard2>
+            </Pressable>
+            <Pressable onPress={() => {navigation.navigate('card', {id:3})}}>
+              <ShortCard3></ShortCard3>
+            </Pressable>
+            <Pressable onPress={() => {navigation.navigate('card', {id:4})}}>
+              <ShortCard4></ShortCard4>
+            </Pressable>
             <View style={{paddingBottom:10}}><TextLink>смотреть все</TextLink></View>
 
             <Heading1>Случайная ссылка</Heading1>
@@ -107,12 +124,157 @@ function Home({navigation}){
 //   )
 // }
 
-function Screen2({navigation}){
+// function SearchScreen({navigation}){
+//   return (
+//     <Stack.Navigator>
+
+//       <Stack.Screen name='cardList' component={CardListScreen}></Stack.Screen>
+//       <Stack.Screen name='card' component={CardScreen}></Stack.Screen>
+
+//       <Stack.Screen name='search result' component={Screen2_1} options={{headerShown:true}}/>
+//       <Stack.Screen name='card name' component={Screen2_2} />
+
+//     </Stack.Navigator>
+//   )
+// }
+
+function CardListScreen({navigation}){
   return (
-    <Stack.Navigator>
-      <Stack.Screen name='search result' component={Screen2_1} options={{headerShown:true}}/>
-      <Stack.Screen name='card name' component={Screen2_2} />
-    </Stack.Navigator>
+    <View style={styles.container}>
+
+      <View style={{ width:"auto", paddingHorizontal:16, paddingVertical:24,
+          flexDirection:"row", alignItems:'center', justifyContent:'center'}}>
+            {/* <Pressable onPress={() => navigation.navigate('home')}>
+                <Image source={require('./assets/arrowleft.png')} style={{width:30, height:30}}/>
+            </Pressable> */}
+            <Search></Search>
+      </View>
+
+      <Pressable onPress={() => {navigation.navigate('card', {id:1})}}>
+        <ShortCard1></ShortCard1>
+      </Pressable>
+      <Pressable onPress={() => {navigation.navigate('card', {id:2})}}>
+        <ShortCard2></ShortCard2>
+      </Pressable>
+      <Pressable onPress={() => {navigation.navigate('card', {id:3})}}>
+        <ShortCard3></ShortCard3>
+      </Pressable>
+      <Pressable onPress={() => {navigation.navigate('card', {id:4})}}>
+        <ShortCard4></ShortCard4>
+      </Pressable>
+      <Pressable onPress={() => {navigation.navigate('card', {id:5})}}>
+        <Text>Card not found</Text>
+      </Pressable>
+      <StatusBar style="auto" />
+
+
+    </View>
+  )
+}
+
+const cards = [
+  {
+    id:1, 
+    link:'https://www.awwwards.com/',
+    pic:`require('./assets/awards.png')`,
+    title: 'Awwwards',
+    text:'Сайт, публикующий интересные сайты, направлен на продвижение лучших инновационных разработок в вебе',
+    tag1:'веб-дизайн', 
+    tag2:'сайт', 
+    tag3:'', 
+    tag4:'', 
+    tag5:'' 
+  },
+  {
+    id:2, 
+    link:'https://cantunsee.space/',
+    pic:`require('./assets/unsee.png')`,
+    title: `Can't Unsee`, 
+    text:'Игра для дизайнеров для проверки знаний в дизайне интерфейсов',
+    tag1:'веб-дизайн', 
+    tag2:'сайт', 
+    tag3:'', 
+    tag4:'', 
+    tag5:'' 
+  },
+  {
+    id:3, 
+    link:'https://hydra.ojack.xyz/',
+    pic:`require('./assets/hydra.png')`,
+    title: 'Hydra: Live Coding Visuals',
+    text:'Среда кодирования с возможностью изменения кода в реальном времени, которая запускается непосредственно в браузере.',
+    tag1:'веб-дизайн', 
+    tag2:'сайт', 
+    tag3:'', 
+    tag4:'', 
+    tag5:'' 
+  },
+  {
+    id:4, 
+    link:'https://www.wiley.com/en-gb/About+Face%3A+The+Essentials+of+Interaction+Design%2C+4th+Edition-p-9781118766583',
+    pic:`require('./assets/aboutface.png')`,
+    title: 'Об интерфейсах: основы интерактивного дизайна',
+    text:'«Об интерфейсах» — это книга, которая вывела интерактивный дизайн из исследовательских лабораторий в повседневный лексикон, а обновленное четвертое издание продолжает лидировать в идеях и методах, актуальных для сегодняшних практиков дизайна и разработчиков.',
+    tag1:'веб-дизайн', 
+    tag2:'сайт', 
+    tag3:'', 
+    tag4:'', 
+    tag5:'' 
+  },
+]
+
+function CardScreen({route}){
+  const {id} = route.params
+  const filteredCards = cards.filter( card => card.id === id ) 
+  let title = '404'
+  let text = 'Карточка не найдена'
+  let tag1, tag2, pic
+  if( filteredCards.length > 0 ){
+    const card = filteredCards[0]
+    title = card.title
+    text = card.text
+    tag1 = card.tag1
+    tag2 = card.tag2
+    pic = card.pic
+  }
+  return (
+    <View style={{paddingHorizontal:10,  borderRadius: 6, overflow: 'hidden', 
+    flexDirection: 'column', display: 'flex'}}>
+
+        <View style={{width: '100%', height: 180, 
+        backgroundColor:'', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+            <Image 
+            source={require()} resizeMode={"contain"}
+            style={{width:"100%", height:"100%", borderRadius: 10, overflow: 'hidden'}}/>
+        </View>
+
+        <View style={{width: '100%', gap: 4,
+        paddingTop: 12, paddingLeft: 12, paddingRight: 12, paddingBottom: 18, 
+        flexDirection: 'column', justifyContent: 'center', alignItems: 'start'}}>
+
+            <View style={{width: '100%', flexDirection: 'column', justifyContent: 'center', alignItems: 'start', display: 'flex'}}>
+
+                <Heading2>
+                    {title}
+                </Heading2>
+
+                <View style={{flexDirection:'row', gap: 4, display: 'flex', alignItems: 'center'}}>
+                    <TagText>{tag1}</TagText>
+                    <View style={{paddingTop:3}}><View style={{ backgroundColor: '#555555', height: 3, width: 3, borderRadius: 100 }}></View></View>
+                    <TagText>{tag2}</TagText>
+                </View>
+
+            </View>
+
+            <View style={{height:1, marginVertical:4, width:60, backgroundColor: '#555555'}}></View>
+
+            <MainText>
+                {text}
+            </MainText>  
+
+        </View>
+
+    </View>
   )
 }
 
--- components/shortCard.js
+++ components/shortCard.js
@@ -98,138 +98,128 @@ export default function ShortCardList(){
 
 export function ShortCard(){
     return(
-        <Pressable onPress={() => console.log('click')}>
-            <View style={{height:100, width: 280, paddingLeft:10, paddingRight:10, justifyContent:'center'}}>
-                <View style={{flexDirection:'row'}}>
-                    <View style={{width: 80, height: 80, padding: 4, display: 'flex', justifyContent: 'center', alignItems:'center'}}>
-                        <View style={{backgroundColor:'gray', flex: 1, alignSelf: 'stretch', borderRadius: 10, overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-                            <Image 
-                                source={require('../assets/awards.png')} 
-                                style={{width:80, height:80}} resizeMode={"contain"}
-                            />
-                        </View>
-                    </View>
-                    <View style={{width: '100%', paddingTop: 12, paddingLeft: 12, paddingRight: 12, paddingBottom: 18, flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', display: 'flex'}}>
-                        <Text style={{width: '100%', color: 'black', fontSize: 20, fontWeight: '600'}} numberOfLines={4} ellipsizeMode='tail'>
-                            Awwwards
-                        </Text>
-                        <Text style={{width: '100%', color: '#555555', fontSize: 14, fontWeight: '300', lineHeight: 21}} numberOfLines={1} ellipsizeMode='tail'>
-                            Сайт, публикующий интересные сайты, направлен на продвижение лучших инновационных разработок в вебе
-                        </Text>
+        <View style={{height:100, width: 280, paddingLeft:10, paddingRight:10, justifyContent:'center'}}>
+            <View style={{flexDirection:'row'}}>
+                <View style={{width: 80, height: 80, padding: 4, display: 'flex', justifyContent: 'center', alignItems:'center'}}>
+                    <View style={{backgroundColor:'gray', flex: 1, alignSelf: 'stretch', borderRadius: 10, overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                        <Image 
+                            source={require('../assets/awards.png')} 
+                            style={{width:80, height:80}} resizeMode={"contain"}
+                        />
                     </View>
                 </View>
+                <View style={{width: '100%', paddingTop: 12, paddingLeft: 12, paddingRight: 12, paddingBottom: 18, flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', display: 'flex'}}>
+                    <Text style={{width: '100%', color: 'black', fontSize: 20, fontWeight: '600'}} numberOfLines={4} ellipsizeMode='tail'>
+                        Awwwards
+                    </Text>
+                    <Text style={{width: '100%', color: '#555555', fontSize: 14, fontWeight: '300', lineHeight: 21}} numberOfLines={1} ellipsizeMode='tail'>
+                        Сайт, публикующий интересные сайты, направлен на продвижение лучших инновационных разработок в вебе
+                    </Text>
+                </View>
             </View>
-        </Pressable>
+        </View>
     )
 }
 
 export function ShortCard1(){
     return(
-        <Pressable onPress={() => console.log('click')}>
-            <View style={{height:100, width: 280, paddingLeft:10, paddingRight:10, justifyContent:'center'}}>
-                <View style={{flexDirection:'row'}}>
-                    <View style={{width: 80, height: 80, padding: 4, display: 'flex', justifyContent: 'center', alignItems:'center'}}>
-                        <View style={{backgroundColor:'gray', flex: 1, alignSelf: 'stretch', borderRadius: 10, overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-                            <Image 
-                                source={require('../assets/awards.png')} 
-                                style={{width:80, height:80}} resizeMode={"contain"}
-                            />
-                        </View>
-                    </View>
-                    <View style={{width: '100%', paddingTop: 12, paddingLeft: 12, paddingRight: 12, paddingBottom: 18, flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', display: 'flex'}}>
-                        <Text style={{width: '100%', color: 'black', fontSize: 20, fontWeight: '600'}} numberOfLines={1} ellipsizeMode='tail'>
-                            Awwwards
-                        </Text>
-                        <Text style={{width: '100%', color: '#555555', fontSize: 14, fontWeight: '300', lineHeight: 21}} numberOfLines={2} ellipsizeMode='tail'>
-                            Сайт, публикующий интересные сайты, направлен на продвижение лучших инновационных разработок в вебе
-                        </Text>
+        <View style={{height:100, width: 280, paddingLeft:10, paddingRight:10, justifyContent:'center'}}>
+            <View style={{flexDirection:'row'}}>
+                <View style={{width: 80, height: 80, padding: 4, display: 'flex', justifyContent: 'center', alignItems:'center'}}>
+                    <View style={{backgroundColor:'gray', flex: 1, alignSelf: 'stretch', borderRadius: 10, overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                        <Image 
+                            source={require('../assets/awards.png')} 
+                            style={{width:80, height:80}} resizeMode={"contain"}
+                        />
                     </View>
                 </View>
+                <View style={{width: '100%', paddingTop: 12, paddingLeft: 12, paddingRight: 12, paddingBottom: 18, flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', display: 'flex'}}>
+                    <Text style={{width: '100%', color: 'black', fontSize: 20, fontWeight: '600'}} numberOfLines={1} ellipsizeMode='tail'>
+                        Awwwards
+                    </Text>
+                    <Text style={{width: '100%', color: '#555555', fontSize: 14, fontWeight: '300', lineHeight: 21}} numberOfLines={2} ellipsizeMode='tail'>
+                        Сайт, публикующий интересные сайты, направлен на продвижение лучших инновационных разработок в вебе
+                    </Text>
+                </View>
             </View>
-        </Pressable>
+        </View>
     )
 }
 
 export function ShortCard2(){
     return(
-        <Pressable onPress={() => console.log('click')}>
-            <View style={{height:100, width: 280, paddingLeft:10, paddingRight:10, justifyContent:'center'}}>
-                <View style={{flexDirection:'row'}}>
-                    <View style={{width: 80, height: 80, padding: 4, display: 'flex', justifyContent: 'center', alignItems:'center'}}>
-                        <View style={{backgroundColor:'gray', flex: 1, alignSelf: 'stretch', borderRadius: 10, overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-                            <Image 
-                                source={require('../assets/unsee.png')} 
-                                style={{width:80, height:80}} resizeMode={"contain"}
-                            />
-                        </View>
-                    </View>
-                    <View style={{width: '100%', paddingTop: 12, paddingLeft: 12, paddingRight: 12, paddingBottom: 18, flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', display: 'flex'}}>
-                        <Text style={{width: '100%', color: 'black', fontSize: 20, fontWeight: '600'}} numberOfLines={1} ellipsizeMode='tail'>
-                            Can't Unsee
-                        </Text>
-                        <Text style={{width: '100%', color: '#555555', fontSize: 14, fontWeight: '300', lineHeight: 21}} numberOfLines={2} ellipsizeMode='tail'>
-                            Игра для дизайнеров для проверки знаний в дизайне интерфейсов
-                        </Text>
+        <View style={{height:100, width: 280, paddingLeft:10, paddingRight:10, justifyContent:'center'}}>
+            <View style={{flexDirection:'row'}}>
+                <View style={{width: 80, height: 80, padding: 4, display: 'flex', justifyContent: 'center', alignItems:'center'}}>
+                    <View style={{backgroundColor:'gray', flex: 1, alignSelf: 'stretch', borderRadius: 10, overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                        <Image 
+                            source={require('../assets/unsee.png')} 
+                            style={{width:80, height:80}} resizeMode={"contain"}
+                        />
                     </View>
                 </View>
+                <View style={{width: '100%', paddingTop: 12, paddingLeft: 12, paddingRight: 12, paddingBottom: 18, flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', display: 'flex'}}>
+                    <Text style={{width: '100%', color: 'black', fontSize: 20, fontWeight: '600'}} numberOfLines={1} ellipsizeMode='tail'>
+                        Can't Unsee
+                    </Text>
+                    <Text style={{width: '100%', color: '#555555', fontSize: 14, fontWeight: '300', lineHeight: 21}} numberOfLines={2} ellipsizeMode='tail'>
+                        Игра для дизайнеров для проверки знаний в дизайне интерфейсов
+                    </Text>
+                </View>
             </View>
-        </Pressable>
+        </View>
     )
 }
 
 export function ShortCard3(){
-    return(
-        <Pressable onPress={() => console.log('click')}>
-            <View style={{height:100, width: 280, paddingLeft:10, paddingRight:10, justifyContent:'center'}}>
-                <View style={{flexDirection:'row'}}>
-                    <View style={{width: 80, height: 80, padding: 4, display: 'flex', justifyContent: 'center', alignItems:'center'}}>
-                        <View style={{backgroundColor:'gray', flex: 1, alignSelf: 'stretch', borderRadius: 10, overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-                            <Image 
-                                source={require('../assets/hydra.png')} 
-                                style={{width:80, height:80}} resizeMode={"contain"}
-                            />
-                        </View>
-                    </View>
-                    <View style={{width: '100%', paddingTop: 12, paddingLeft: 12, paddingRight: 12, paddingBottom: 18, flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', display: 'flex'}}>
-                        <Text style={{width: '100%', color: 'black', fontSize: 20, fontWeight: '600'}} numberOfLines={1} ellipsizeMode='tail'>
-                            Hydra: Live Coding Visuals
-                        </Text>
-                        <Text style={{width: '100%', color: '#555555', fontSize: 14, fontWeight: '300', lineHeight: 21}} numberOfLines={2} ellipsizeMode='tail'>
-                            Среда кодирования с возможностью изменения кода в реальном времени, которая запускается непосредственно в браузере
-                        </Text>
+    return(    
+        <View style={{height:100, width: 280, paddingLeft:10, paddingRight:10, justifyContent:'center'}}>
+            <View style={{flexDirection:'row'}}>
+                <View style={{width: 80, height: 80, padding: 4, display: 'flex', justifyContent: 'center', alignItems:'center'}}>
+                    <View style={{backgroundColor:'gray', flex: 1, alignSelf: 'stretch', borderRadius: 10, overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                        <Image 
+                            source={require('../assets/hydra.png')} 
+                            style={{width:80, height:80}} resizeMode={"contain"}
+                        />
                     </View>
                 </View>
+                <View style={{width: '100%', paddingTop: 12, paddingLeft: 12, paddingRight: 12, paddingBottom: 18, flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', display: 'flex'}}>
+                    <Text style={{width: '100%', color: 'black', fontSize: 20, fontWeight: '600'}} numberOfLines={1} ellipsizeMode='tail'>
+                        Hydra: Live Coding Visuals
+                    </Text>
+                    <Text style={{width: '100%', color: '#555555', fontSize: 14, fontWeight: '300', lineHeight: 21}} numberOfLines={2} ellipsizeMode='tail'>
+                        Среда кодирования с возможностью изменения кода в реальном времени, которая запускается непосредственно в браузере
+                    </Text>
+                </View>
             </View>
-        </Pressable>
+        </View>
     )
 }
 
 export function ShortCard4(){
     return(
-        <Pressable onPress={() => console.log('click')}>
-            <View style={{height:100, width: 280, paddingLeft:10, paddingRight:10, justifyContent:'center'}}>
-                <View style={{flexDirection:'row'}}>
+        <View style={{height:100, width: 280, paddingLeft:10, paddingRight:10, justifyContent:'center'}}>
+            <View style={{flexDirection:'row'}}>
 
-                    <View style={{width: 80, height: 80, padding: 4, display: 'flex', justifyContent: 'center', alignItems:'center'}}>
-                        <View style={{backgroundColor:'#eee', flex: 1, alignSelf: 'stretch', borderRadius: 10, overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-                            <Image 
-                                source={require('../assets/aboutface.png')} 
-                                style={{width: 80, height: 80}} resizeMode={"contain"}
-                            />
-                        </View>
+                <View style={{width: 80, height: 80, padding: 4, display: 'flex', justifyContent: 'center', alignItems:'center'}}>
+                    <View style={{backgroundColor:'#eee', flex: 1, alignSelf: 'stretch', borderRadius: 10, overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                        <Image 
+                            source={require('../assets/aboutface.png')} 
+                            style={{width: 80, height: 80}} resizeMode={"contain"}
+                        />
                     </View>
-
-                    <View style={{width: '100%', paddingTop: 12, paddingLeft: 12, paddingRight: 12, paddingBottom: 18, flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', display: 'flex'}}>
-                        <Text style={{width: '100%', color: 'black', fontSize: 20, fontWeight: '600'}} numberOfLines={1} ellipsizeMode='tail'>
-                            About Face
-                        </Text>
-                        <Text style={{width: '100%', color: '#555555', fontSize: 14, fontWeight: '300', lineHeight: 21}} numberOfLines={2} ellipsizeMode='tail'>
-                            «Об интерфейсах» — это книга, которая вывела интерактивный дизайн из исследовательских лабораторий в повседневный лексикон, а обновленное четвертое издание продолжает лидировать в идеях и методах, актуальных для сегодняшних практиков дизайна и разработчиков
-                        </Text>
-                    </View>
-
                 </View>
+
+                <View style={{width: '100%', paddingTop: 12, paddingLeft: 12, paddingRight: 12, paddingBottom: 18, flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', display: 'flex'}}>
+                    <Text style={{width: '100%', color: 'black', fontSize: 20, fontWeight: '600'}} numberOfLines={1} ellipsizeMode='tail'>
+                        About Face
+                    </Text>
+                    <Text style={{width: '100%', color: '#555555', fontSize: 14, fontWeight: '300', lineHeight: 21}} numberOfLines={2} ellipsizeMode='tail'>
+                        «Об интерфейсах» — это книга, которая вывела интерактивный дизайн из исследовательских лабораторий в повседневный лексикон, а обновленное четвертое издание продолжает лидировать в идеях и методах, актуальных для сегодняшних практиков дизайна и разработчиков
+                    </Text>
+                </View>
+
             </View>
-        </Pressable>
+        </View>
     )
 }
